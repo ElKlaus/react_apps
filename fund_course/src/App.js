f7ca9c57fa8a594bb5ff9702890c7cbc6994f275
ElKlaus <klaus15rus@gmail.com>
2023-01-20 19:43:10 +0300

    React hooks

--- fund_course/src/App.js
+++ fund_course/src/App.js
@@ -1,22 +1,15 @@
 import React, {useState} from "react";
+import ClassCounter from "./components/ClassCounter";
+import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import './styles/App.css';
 
 function App() {
-  const [likes, setLikes] = useState(7);
   const [value, setValue] = useState('ТЕКСТ В INPUT');
-
-  function increment() {
-    setLikes(likes + 1);
-  }
-
-  function dicrement() {
-    setLikes(likes - 1);
-  }
 
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={dicrement}>Direment</button>
+      <PostItem/>
     </div>
   );
 }
