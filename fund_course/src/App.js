08b5ac4795a87c4707d5fd377a3618c45e9423b5
ElKlaus <klaus15rus@gmail.com>
2023-01-21 19:21:15 +0300

    Uncontroll components - UseRef()

--- fund_course/src/App.js
+++ fund_course/src/App.js
@@ -1,17 +1,50 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 import './styles/App.css';
 
 function App() {
-  const [value, setValue] = useState('ТЕКСТ В INPUT');
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript', body: 'Description'},
+    {id: 2, title: 'Javascript 2', body: 'Description'},
+    {id: 3, title: 'Javascript 3', body: 'Description'},
+  ])
+
+  const [title, setTitle] = useState('');
+  const bodyInputRef = useRef();
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+
+    console.log(title);
+    console.log(bodyInputRef.current.value);
+  }
 
   return (
     <div className="App">
-      <PostItem/>
+      <form>
+        {/* Управляемый компонент */}
+        <MyInput
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type="text"
+          placeholder="Название поста"
+        />
+        {/* Неуправляемый/неконтролируемый компонент */}
+        <MyInput
+          ref={bodyInputRef}
+          type="text"
+          placeholder="Описание поста"
+        />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title='Посты про JS'/>
     </div>
-  );
+  ) 
 }
 
 export default App;
