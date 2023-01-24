bdb44a5b9b4815c3d69fd18a02f51031ae4c0814
ElKlaus <klaus15rus@gmail.com>
2023-01-24 14:47:17 +0300

    Create search

--- fund_course/src/App.js
+++ fund_course/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from "react";
+import React, {useState, useMemo, useRef} from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 import PostForm from "./components/PostForm";
@@ -17,6 +17,19 @@ function App() {
   ])
 
   const [selectedSort, setSelectedSort] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const sortedPosts = useMemo(() => {
+    if(selectedSort) {
+      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+    }
+
+    return posts;    
+  }, [selectedSort, posts]);
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery));
+  }, [searchQuery, sortedPosts])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -29,8 +42,6 @@ function App() {
 
   const sortPosts = (sort) => {
     setSelectedSort(sort)
-
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
   }
 
   return (
@@ -38,6 +49,11 @@ function App() {
       <PostForm create={createPost}/>
       <hr style={{margin: '15px 0'}}/>
       <div>
+        <MyInput
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+          placeholder="Поиск..."
+        />
         <MySelect
           value={selectedSort}
           onChange={sortPosts}
@@ -50,9 +66,9 @@ function App() {
       </div>
       {/* Условная отрисовка */}
       {
-        posts.length
+        sortedAndSearchedPosts.length
           ?
-          <PostList remove={removePost} posts={posts} title='Посты про JS'/>
+          <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Посты про JS'/>
           :
           <h1 style={{textAlign: 'center'}}>
             Посты не найдены
