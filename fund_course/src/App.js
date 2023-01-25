823262894d4739aaeb15644d04439e146cb84030
ElKlaus <klaus15rus@gmail.com>
2023-01-25 13:42:26 +0300

    Create comp MyModal

--- fund_course/src/App.js
+++ fund_course/src/App.js
@@ -1,11 +1,13 @@
 import React, {useState, useMemo, useRef} from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
+import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import MyModal from "./components/UI/MyModal/MyModal";
 import MySelect from "./components/UI/select/MySelect";
 import './styles/App.css';
 
@@ -16,23 +18,24 @@ function App() {
     {id: 3, title: 'вв 3', body: 'рр'},
   ])
 
-  const [selectedSort, setSelectedSort] = useState('');
-  const [searchQuery, setSearchQuery] = useState('');
+  const [filter, setFilter] = useState({sort: '', query: ''});
+  const [modal, setModal] = useState(false);
 
   const sortedPosts = useMemo(() => {
-    if(selectedSort) {
-      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+    if(filter.sort) {
+      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
     }
 
     return posts;    
-  }, [selectedSort, posts]);
+  }, [filter.sort, posts]);
 
   const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery));
-  }, [searchQuery, sortedPosts])
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query));
+  }, [filter.query, sortedPosts])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
+    setModal(false);
   }
 
   // Получаем post из дочернего компонента
@@ -40,40 +43,20 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id));
   }
 
-  const sortPosts = (sort) => {
-    setSelectedSort(sort)
-  }
-
   return (
     <div className="App">
-      <PostForm create={createPost}/>
+      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+        Создать пользователя
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal} >
+        <PostForm create={createPost}/>
+      </MyModal>
       <hr style={{margin: '15px 0'}}/>
-      <div>
-        <MyInput
-          value={searchQuery}
-          onChange={e => setSearchQuery(e.target.value)}
-          placeholder="Поиск..."
-        />
-        <MySelect
-          value={selectedSort}
-          onChange={sortPosts}
-          defaultValue="Сортировка"
-          options={[
-            {value: 'title', name: 'По названию'},
-            {value: 'body', name: 'По описанию'},
-          ]}
-        />
-      </div>
-      {/* Условная отрисовка */}
-      {
-        sortedAndSearchedPosts.length
-          ?
-          <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Посты про JS'/>
-          :
-          <h1 style={{textAlign: 'center'}}>
-            Посты не найдены
-          </h1>
-      }      
+      <PostFilter
+        filter={filter}
+        setFilter={setFilter}
+      />
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Посты про JS'/>     
     </div>
   )
 }
--- fund_course/src/components/PostList.jsx
+++ fund_course/src/components/PostList.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import PostItem from "./PostItem";
 
 const PostList = ({posts, title, remove}) => {
+  if (!posts.length) {
+    return (
+      <h1 style={{textAlign: 'center'}}>
+        Посты не найдены
+      </h1>
+    )
+  }
+
   return (
     <div className="App">
       <h1 style={{textAlign: 'center'}}>
