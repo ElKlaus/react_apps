0adbe18fcb9006037e30b151e22efba792cb50e7
ElKlaus <klaus15rus@gmail.com>
2023-01-26 17:22:55 +0300

    create Loader

--- fund_course/src/App.js
+++ fund_course/src/App.js
@@ -1,4 +1,6 @@
-import React, {useState, useMemo, useRef} from "react";
+import React, {useState, useMemo, useRef, useEffect} from "react";
+import PostService from "./API/PostService";
+import axios from 'axios';
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 import PostFilter from "./components/PostFilter";
@@ -9,33 +11,36 @@ import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MySelect from "./components/UI/select/MySelect";
+import { usePosts } from "./hooks/usePosts";
 import './styles/App.css';
+import Loader from "./components/UI/Loader/Loader";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'aa', body: 'бб'},
-    {id: 2, title: 'гг 2', body: 'пп'},
-    {id: 3, title: 'вв 3', body: 'рр'},
-  ])
-
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostsLoading, setIsPostLoading] = useState(false);
 
-  const sortedPosts = useMemo(() => {
-    if(filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-
-    return posts;    
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query));
-  }, [filter.query, sortedPosts])
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
+  };
+
+  async function fetchPosts() {
+    setIsPostLoading(true);
+    setTimeout(async () => {
+      const posts = await axios.get('https://jsonplaceholder.typicode.com/posts');
+
+      setPosts(posts.data);
+      setIsPostLoading(false);
+    }, 2000);
+    // const posts = await PostService.getAll();
+
   }
 
   // Получаем post из дочернего компонента
@@ -56,7 +61,10 @@ function App() {
         filter={filter}
         setFilter={setFilter}
       />
-      <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Посты про JS'/>     
+      {isPostsLoading
+        ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div>
+        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Посты про JS'/>
+      }
     </div>
   )
 }
