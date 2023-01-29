c1dfa60e3a2e300cd7d31c34c40d5379aa9fc2a2
ElKlaus <klaus15rus@gmail.com>
2023-01-29 16:09:03 +0300

    PostIdPage finished

--- fund_course/src/pages/PostIdPage.jsx
+++ fund_course/src/pages/PostIdPage.jsx
@@ -7,15 +7,21 @@ import { useFetching } from '../hooks/useFetching';
 export const PostIdPage = () => {
   const params = useParams();
   const [post, setPost] = useState({});
+  const [comments, setComments] = useState([]);
   const [fetchPostById, isLoading, error] = useFetching(async (id) => {
     const response = await PostService.getById(id);
 
     setPost(response.data);
   });
+  const [fetchComments, isCommLoading, comError] = useFetching(async (id) => {
+    const response = await PostService.getCommentByPostId(id);
 
+    setComments(response.data);
+  });
 
   useEffect(() => {
     fetchPostById(params.id);
+    fetchComments(params.id);
   }, []);
 
   return (
@@ -24,7 +30,21 @@ export const PostIdPage = () => {
       {isLoading
         ? <Loader/>
         : <div>{post.id}. {post.title}</div>
-      }      
+      }
+      <h1>
+        Комментарии
+      </h1>
+      {isCommLoading
+        ? <Loader/>
+        : <div>
+          {comments.map(comm => 
+            <div style={{marginTop: 15}}>
+              <h5>{comm.email}</h5>
+              <div>{comm.body}</div>
+            </div>
+          )}
+        </div>
+      }
     </div>
   )
 }
